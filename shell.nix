f525c5e0956d29c9b98f4ac947bf5951a4f4d610
Benjamin Summers <ben@benjaminsummers.net>
2015-03-11 22:23:29 +0000

    Rewrite; correctly handle parsing and autoconf configuration.

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-let haskellPackages = pkgs.haskellPackages.override {
-      extension = self: super: {
-        preprocessHaskell = self.callPackage ./. {};
-      };
-    };
- in lib.overrideDerivation haskellPackages.preprocessHaskell (attrs: {
-      buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
-    })
